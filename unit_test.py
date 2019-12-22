4d447bc176e0fc296eeb3663e38817964617132e
Apple <apple@Apples-MacBook-Pro.local>
2019-12-22 18:48:58 +0530

    added unit test cases

--- unit_test.py
+++ unit_test.py
@@ -71,7 +71,7 @@ class VendingMachineUnitTesting(unittest.TestCase):
         item_detail = list(filter(lambda x: x["name"] == name, vm_m.ITEM_LIST))
         item_detail = item_detail[0]
         new_quantity = item_detail.get("quantity")
-        self.assertEqual(True, previous_quantity != new_quantity)
+        self.assertEqual(True, previous_quantity < new_quantity)
 
     def test_admin_update_quantity_with_incorrect_data(self):
         vm_m = VendingMachine()
@@ -84,31 +84,59 @@ class VendingMachineUnitTesting(unittest.TestCase):
         item_detail = list(filter(lambda x: x["name"] == name, vm_m.ITEM_LIST))
         item_detail = item_detail[0]
         new_quantity = item_detail.get("quantity")
-        self.assertEqual(False, previous_quantity != new_quantity)
+        self.assertEqual(False, previous_quantity < new_quantity)
 
-    # def test_admin_add_stock_with_correct_data(self):
-    #     vm_m = VendingMachine()
-    #     self.assertEqual(True, )
-    #
-    # def test_admin_add_stock_with_incorrect_data(self):
-    #     vm_m = VendingMachine()
-    #     self.assertEqual(False, )
-    #
-    # def test_admin_add_vending_machine_coins_with_correct_data(self):
-    #     vm_m = VendingMachine()
-    #     self.assertEqual(True, )
-    #
-    # def test_admin_add_vending_machine_coins_with_incorrect_data(self):
-    #     vm_m = VendingMachine()
-    #     self.assertEqual(False, )
-    #
-    # def test_add_coin_with_correct_data(self):
-    #     vm_m = VendingMachine()
-    #     self.assertEqual(True, )
-    #
-    # def test_add_coin_with_incorrect_data(self):
-    #     vm_m = VendingMachine()
-    #     self.assertEqual(False, )
+    def test_admin_add_stock_with_correct_data(self):
+        vm_m = VendingMachine()
+        name = "Blueberry"
+        vm_m.admin_add_stock({"name": name, "quantity": 0, "price": 30})
+        item_detail = list(filter(lambda x: x["name"] == name, vm_m.ITEM_LIST))
+        if item_detail and len(item_detail) > 0:
+            item_detail = item_detail[0]
+            item = item_detail.get("name")
+        else:
+            item = None
+        self.assertEqual(True, item is not None)
+
+    def test_admin_add_stock_with_incorrect_data(self):
+        vm_m = VendingMachine()
+        name = "Blackberry"
+        vm_m.admin_add_stock({"price": 30})
+        item_detail = list(filter(lambda x: x["name"] == name, vm_m.ITEM_LIST))
+        if item_detail and len(item_detail) > 0:
+            item_detail = item_detail[0]
+            item = item_detail.get("name")
+        else:
+            item = None
+        self.assertEqual(False, item is not None)
+
+    def test_admin_add_vending_machine_coins_with_correct_data(self):
+        vm_m = VendingMachine()
+        previous_count_10_coin = vm_m.coins.get("10")
+        vm_m.admin_add_vending_machine_coins({"10": 100})
+        new_count_10_coin = vm_m.coins.get("10")
+        self.assertEqual(True, new_count_10_coin > previous_count_10_coin)
+
+    def test_admin_add_vending_machine_coins_with_incorrect_data(self):
+        vm_m = VendingMachine()
+        previous_count_10_coin = vm_m.coins.get("10")
+        vm_m.admin_add_vending_machine_coins({"10": 0})
+        new_count_10_coin = vm_m.coins.get("10")
+        self.assertEqual(False, new_count_10_coin > previous_count_10_coin)
+
+    def test_add_coin_with_correct_data(self):
+        vm_m = VendingMachine()
+        previous_count_10_coin = vm_m.provisional_coin_storage.get("10")
+        vm_m.add_coin("10")
+        new_count_10_coin = vm_m.provisional_coin_storage.get("10")
+        self.assertEqual(True, new_count_10_coin > previous_count_10_coin)
+
+    def test_add_coin_with_incorrect_data(self):
+        vm_m = VendingMachine()
+        previous_count_10_coin = vm_m.provisional_coin_storage.get("10")
+        vm_m.add_coin("1000")
+        new_count_10_coin = vm_m.provisional_coin_storage.get("10")
+        self.assertEqual(False, new_count_10_coin > previous_count_10_coin)
 
     # ----------------------------------- #
 
